8ab8f625b3784ac34e2b7017d573eaa92ae0e949
WangGang <wanggang@bestwise.cc>
2019-12-30 20:11:28 +0800

    添加项目文件。

---
+++ Web/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/Hanlth")]
+    public class HealthController : Controller
+    {
+        [HttpGet]
+        public ActionResult get() => Ok("ok");
+    }
+}
